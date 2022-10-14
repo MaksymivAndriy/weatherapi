118f518fa712a48ea9af97cc3d302d732ec5f251
Andriy Maksymiv <guspisia@gmail.com>
2022-10-14 21:20:26 +0200

    fod

--- main.tf
+++ main.tf
@@ -1,9 +1,5 @@
 provider "azurerm" {
   version     = "2.85.0"
-  subscription_id = "457b89a8-82bc-4337-a34c-dff14982f42d"
-  client_id       = "688007ee-10a5-457b-9d21-0bce214943b3"
-  client_secret   = "DId8Q~1cFcUnWM.CA0NMcRAYGDF_nRSn7.ZvdbWP"
-  tenant_id       = "be387676-1096-4531-91c8-a16c66db0b57"
   features {}
 }
 
